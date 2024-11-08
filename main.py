c82c6da77d5507e769194592ef4d80d85d3cd706
evansricky20 <77314096+evansricky20@users.noreply.github.com>
2024-11-07 20:37:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,173 +1,80 @@
+import random
+
 import pygame
 import time
-
+import numpy as np
 from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-import cube
+sphereList = []
 
-verticies = (
-   (1, -1, -1),
-   (1, 1, -1),
-   (-1, 1, -1),
-   (-1, -1, -1),
-   (1, -1, 1),
-   (1, 1, 1),
-   (-1, -1, 1),
-   (-1, 1, 1)
-)
-
-edges = (
-   (0,1),
-   (0,3),
-   (0,4),
-   (2,1),
-   (2,3),
-   (2,7),
-   (6,3),
-   (6,4),
-   (6,7),
-   (5,1),
-   (5,4),
-   (5,7)
-)
-
-class Ground:
-   def __init__(self, width, depth):
-      self.width = width
-      self.depth = depth
-
-   def paint(self):
-      glColor3f(0.2, 0.5, 0.2)
-      glPushMatrix()
-      glTranslatef(0, 0, 0)
-      glScalef(self.width, 0.1, self.depth)
-
-      glBegin(GL_QUADS)
-      glVertex3f(-1, 0, -1)
-      glVertex3f(1, 0, -1)
-      glVertex3f(1, 0, 1)
-      glVertex3f(-1, 0, 1)
-      glEnd()
-
-      glPopMatrix()
-
-class Cube:
-   def __init__(self, name, pos_x, pos_y, pos_z, height, width, depth):
+class Sphere:
+   def __init__(self, name, radius, x_cord, y_cord, z_cord):
       self.name = name
-      self.pos_x = pos_x
-      self.pos_y = pos_y
-      self.pos_z = pos_z
-      self.height = height
-      self.width = width
-      self.depth = depth
-
-   def paint(self):
-      #glColor3f(0.8, 0.2, 0.2)
-      glPushMatrix()
-
-      glTranslatef(self.pos_x, self.pos_y, self.pos_z)
-      glScalef(self.width, self.height, self.depth)
-
-      glBegin(GL_QUADS)
-
-      # Front face
-      glVertex3f(-0.5, -0.5, 0.5)
-      glVertex3f(0.5, -0.5, 0.5)
-      glVertex3f(0.5, 0.5, 0.5)
-      glVertex3f(-0.5, 0.5, 0.5)
-
-      # Back face
-      glVertex3f(-0.5, -0.5, -0.5)
-      glVertex3f(-0.5, 0.5, -0.5)
-      glVertex3f(0.5, 0.5, -0.5)
-      glVertex3f(0.5, -0.5, -0.5)
-
-      # Left face
-      glVertex3f(-0.5, -0.5, -0.5)
-      glVertex3f(-0.5, -0.5, 0.5)
-      glVertex3f(-0.5, 0.5, 0.5)
-      glVertex3f(-0.5, 0.5, -0.5)
-
-      # Right face
-      glVertex3f(0.5, -0.5, -0.5)
-      glVertex3f(0.5, 0.5, -0.5)
-      glVertex3f(0.5, 0.5, 0.5)
-      glVertex3f(0.5, -0.5, 0.5)
-
-      # Top face
-      glVertex3f(-0.5, 0.5, -0.5)
-      glVertex3f(0.5, 0.5, -0.5)
-      glVertex3f(0.5, 0.5, 0.5)
-      glVertex3f(-0.5, 0.5, 0.5)
-
-      # Bottom face
-      glVertex3f(-0.5, -0.5, -0.5)
-      glVertex3f(-0.5, -0.5, 0.5)
-      glVertex3f(0.5, -0.5, 0.5)
-      glVertex3f(0.5, -0.5, -0.5)
-      glEnd()
-
-      glPopMatrix()
-
-class Character:
-   def __init__(self, name, x_cord, y_cord, z_cord):
-      self.name = name
+      self.radius = radius
       self.x_cord = x_cord
       self.y_cord = y_cord
       self.z_cord = z_cord
+      sphereList.append(self)
+      self.red = random.uniform(0, 1)
+      self.green = random.uniform(0, 1)
+      self.blue = random.uniform(0, 1)
 
    def draw(self):
-      #glTranslatef(1, 0.0, 0.0)
-      glColor3f(0.9, 0.1, 0.1)
-      head = Cube("head", self.x_cord, self.y_cord+1, self.z_cord-5, 0.5, 0.5, 0.5)
-      head.paint()
+      glColor3f(self.red, self.green, self.blue)
+      glPushMatrix()
+      glTranslatef(self.x_cord, self.y_cord, self.z_cord)
+      sphere = gluNewQuadric()
+      gluSphere(sphere, self.radius, 32, 32)
+      glPopMatrix()
 
-      glColor3f(0.1, 0.9, 0.1)
-      body = Cube("body", self.x_cord, self.y_cord, self.z_cord-5, 1.5, 1, 0.5)
-      body.paint()
 
-      glColor3f(0.1, 0.1, 0.9)
-      left_arm = Cube("left_arm", self.x_cord-0.8, self.y_cord, self.z_cord-5, 1, 0.5, 0.5)
-      left_arm.paint()
+   def move(self, leftFlag, rightFlag, upFlag, downFlag, spaceFlag, ctrlFlag):
+      if leftFlag == 1:  # if left key bind is active, then subtract from characters x coordiante to move left
+         self.x_cord = self.x_cord - 0.05
+      if rightFlag == 1:  # Opposite if moving right
+         self.x_cord = self.x_cord + 0.05
+      if upFlag == 1:
+         self.z_cord = self.z_cord - 0.05
+      if downFlag == 1:
+         self.z_cord = self.z_cord + 0.05
+      if spaceFlag == 1:
+         self.y_cord = self.y_cord + 0.05
+      if ctrlFlag == 1:
+         self.y_cord = self.y_cord - 0.05
 
-      glColor3f(0.1, 0.1, 0.9)
-      right_arm = Cube("right_arm", self.x_cord+0.8, self.y_cord, self.z_cord-5, 1, 0.5, 0.5)
-      right_arm.paint()
 
-      glColor3d(0.9, 0.1, 0.9)
-      left_leg = Cube("left_leg", self.x_cord-0.3, self.y_cord-1.3, self.z_cord-5, 1, 0.5, 0.5)
-      left_leg.paint()
+def createPlanet():
+   planet = Sphere(f"planet_{random.randint}", 1, 0, 0, 0)
 
-      glColor3d(0.9, 0.1, 0.9)
-      right_leg = Cube("right_leg", self.x_cord+0.3, self.y_cord-1.3, self.z_cord-5, 1, 0.5, 0.5)
-      right_leg.paint()
 
+
+# Main
+#
+# Create opengl renderings
 def main():
    pygame.init()
-   display = (800,600)
+   display = (1000,600)
    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
    gluPerspective(90, (display[0]/display[1]), 0.1, 50.0)
+   gluLookAt(0.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
-   gluLookAt(0.0, 6.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-
-   ground = Ground(20, 20)
-   cube1 = Cube("Cube1", 0, 0, -5, 1, 1, 1)
-   #glTranslatef(19, 0, 0
-   glTranslatef(0, 2, 0)
-
-   bob_x = 0.0
-   bob_y = 0.0
-   bob_z = 0.0
-
+   # Flags to get status of bob
    upFlag = 0
    downFlag = 0
    leftFlag = 0
    rightFlag  = 0
    spaceFlag = 0
+   ctrlFlag = 0
+   activePlanetIndex = 0
+
+   ball = Sphere("ball1", 1, 2, 2, 0)
+   ball2 = Sphere("ball2", 2, 1, 1, 0)
+
+   activePlanet = sphereList[activePlanetIndex]
+   #print(sphereList[0].name)
 
    while True:
       for event in pygame.event.get():
@@ -175,59 +82,105 @@ def main():
             pygame.quit()
             quit()
 
-         keys = pygame.key.get_pressed()
+         # Using pygame keys to find keys being pressed
+         # If specified key is pressed, its flag is set
          if event.type == pygame.KEYDOWN:
-            if keys[pygame.K_LEFT]:
-               print("Left pressed")
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+               #print("Left pressed")
                leftFlag = 1
-            if keys[pygame.K_RIGHT]:
-               print("Right pressed")
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+               #print("Right pressed")
                rightFlag = 1
-            if keys[pygame.K_UP]:
-               print("Up pressed")
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+               #print("Up pressed")
                upFlag = 1
-            if keys[pygame.K_DOWN]:
-               print("Down pressed")
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+               #print("Down pressed")
                downFlag = 1
-            if keys[pygame.K_SPACE]:
-               print("Space pressed")
+            if event.key == pygame.K_SPACE:
+               #print("Space pressed")
                spaceFlag = 1
+            if event.key == pygame.K_LCTRL:
+               #print("CTRL pressed")
+               ctrlFlag  = 1
+            if event.key == pygame.K_COMMA:
+               print("Prev pressed")
+               if activePlanetIndex == 0:
+                  print("Current index 0, cannot go out of range.")
+               else:
+                  activePlanetIndex = activePlanetIndex - 1
+                  activePlanet = sphereList[activePlanetIndex]
+               print(activePlanet.name)
+            if event.key == pygame.K_PERIOD:
+               print("Next pressed")
+               if activePlanetIndex == len(sphereList)-1:
+                  print(f"Current index is {activePlanetIndex}, cannot go out of range.")
+               else:
+                  activePlanetIndex = activePlanetIndex + 1
+                  activePlanet = sphereList[activePlanetIndex]
+               print(activePlanet.name)
 
 
+         # Using pygame keys to find keys released after being pressed
+         # If specified key is released its flag is reset
          if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-               print("Left released")
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+               #print("Left released")
                leftFlag = 0
-            if event.key == pygame.K_RIGHT:
-               print("Right released")
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+               #print("Right released")
                rightFlag = 0
-            if event.key == pygame.K_UP:
-               print("Up released")
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+               #print("Up released")
                upFlag = 0
-            if event.key == pygame.K_DOWN:
-               print("Down released")
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+               #print("Down released")
                downFlag = 0
             if event.key == pygame.K_SPACE:
-               print("Space released")
+               #print("Space released")
                spaceFlag = 0
+            if event.key == pygame.K_LCTRL:
+               #print("CTRL released")
+               ctrlFlag  = 0
 
-      if leftFlag == 1:
-         bob_x = bob_x - 0.05
-      if rightFlag == 1:
-         bob_x = bob_x + 0.05
-      if upFlag == 1:
-         bob_z = bob_z - 0.05
-      if downFlag == 1:
-         bob_z = bob_z + 0.05
-      if spaceFlag == 1:
-         bob_y = bob_y + 1
-         time.sleep(0.5)
-         bob_y = bob_y - 1
+      #ball.move(leftFlag, rightFlag, upFlag, downFlag, spaceFlag)
+      for i in sphereList:
+         if i.name == activePlanet.name:
+            i.move(leftFlag, rightFlag, upFlag, downFlag, spaceFlag, ctrlFlag)
 
-      bob = Character("bob", bob_x, bob_y, bob_z)
+      #time = pygame.time.get_ticks()/1000 # returns time in miliseconds / 1000 to get seconds
+      #print(time)
+      #sinTest = np.sin(time) # test sin
+      #print(sinTest)
+
       glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-      ground.paint()
-      bob.draw()
+      glEnable(GL_DEPTH_TEST)
+
+      # Creating the ground
+      glColor3f(0.2, 0.5, 0.2)
+      glPushMatrix()
+      glTranslatef(0, 0, 0)
+      glScalef(20, 0.1, 20)
+
+      glBegin(GL_QUADS)
+      glVertex3f(-1, 0, -1)
+      glVertex3f(1, 0, -1)
+      glVertex3f(1, 0, 1)
+      glVertex3f(-1, 0, 1)
+      glEnd()
+      glPopMatrix()
+
+      #glColor3f(1, 0, 0)
+      #glPushMatrix()
+      #ball.draw()
+      #ball.updatePos(move_x, move_y, move_z)
+      #glPopMatrix()
+
+      for i in sphereList:
+         glPushMatrix()
+         i.draw()
+         glPopMatrix()
+
       pygame.display.flip()
       pygame.time.wait(10)
 
